8194b0255380f2152ce1d0692fce50a436c39d98
Bhagyashree Malandkar <83403735+Bhagyashree8980@users.noreply.github.com>
2022-01-13 00:37:12 +0530

    Update main.py

--- appengine/flexible/hello_world/main.py
+++ appengine/flexible/hello_world/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'BHAGYASHREE - SMIT2122004 \n BHUSHAN - SMIT2122005 \n CHINMAY - SMIT2122006 \n DEMO FOR THE TOPIC GOOGLE APP ENGINE.'
+    return ['BHAGYASHREE - SMIT2122004',+,'\n',+, 'BHUSHAN - SMIT2122005',+,'\n',+, 'CHINMAY - SMIT2122006',+,'\n',+, 'DEMO FOR THE TOPIC GOOGLE APP ENGINE.']
 
 
 if __name__ == '__main__':
