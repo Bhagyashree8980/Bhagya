7eb71bc1eeb47becf78f74e8b47cbf8e43235d6f
Bhagyashree Malandkar <83403735+Bhagyashree8980@users.noreply.github.com>
2022-01-13 00:19:57 +0530

    Update main.py

--- appengine/flexible/hello_world/main.py
+++ appengine/flexible/hello_world/main.py
@@ -1,18 +1,4 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# [START gae_flex_quickstart]
 from flask import Flask
 
 
@@ -22,11 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'BHAGYASHREE - SMIT2122004 \n BHUSHAN - SMIT2122005 \n CHINMAY - SMIT2122006 \n DEMO FOR THE TOPIC GOOGLE APP ENGINE.'
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app.
+  
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_flex_quickstart]
